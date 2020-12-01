c3d9f320c655752cfd2e72e2a3c753643f87b232
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2020-12-01 12:36:55 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Create React App
 
 Stopwatch
+Timer
--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,47 @@
-import React, { Component } from 'react';
-import Timer from './Timer';
-import './App.css';
-import SwitchButton from './SwitchButton';
-
-
+import React, { Component } from "react";
+import Timer from "./Timer";
+import "./App.css";
+import SwitchButton from "./SwitchButton";
 
 class App extends Component {
-  state = { 
+  state = {
     time: 0,
-    active: false
-   }
+    active: false,
+  };
 
-handleClick = () => {
-  console.log('klick');
-  if(this.state.active){
-    clearInterval(this.idInterval)
-  } else {
-    this.idInterval = setInterval(() => this.addSecond(), 1000)
-  }
+  handleClick = () => {
+    console.log("klick");
+    if (this.state.active) {
+      clearInterval(this.idInterval);
+    } else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000);
+    }
 
-this.setState({
-  active:!this.state.active
-})
-}
+    this.setState({
+      active: !this.state.active,
+    });
+  };
 
-addSecond = ()=> {
-  this.setState({
-    time: this.state.time + 1
-  })
-}
+  addSecond = () => {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  };
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <>
-      <h3>Stopwatch</h3>
-      <p className="timer">{this.state.time}</p>
-      <SwitchButton click={this.handleClick} active={this.state.active} /> <br/>
-      <Timer />
+        <h3>Stopwatch</h3>
+        <p className="timer">{this.state.time}</p>
+        <SwitchButton
+          click={this.handleClick}
+          active={this.state.active}
+        />{" "}
+        <br />
+        <Timer />
       </>
-     );
+    );
   }
 }
- 
+
 export default App;
-
-
-
